26948e383892ae818d0d6803201ab0d1ed4ff4fd
Max Metral <max@pyralis.com>
2017-08-18 12:17:14 -0400

    Fix OR regex to allow A+6 digits

--- test.js
+++ test.js
@@ -5,5 +5,10 @@ var usdl = require('./index');
 tap.test('Should validate numbers', function (t) {
   t.ok(usdl.isValid('MA', 'A12334567'), 'Should be valid');
   t.ok(!usdl.isValid('MA', 'SOFAKE'), 'Should be invalid');
+
+  t.ok(usdl.isValid('OR', 'A123456'), 'Should be valid');
+  t.ok(!usdl.isValid('OR', 'A1234567'), 'Should not be valid');
+  t.ok(!usdl.isValid('OR', 'B123456'), 'Should not be valid');
+
   t.end();
 });
